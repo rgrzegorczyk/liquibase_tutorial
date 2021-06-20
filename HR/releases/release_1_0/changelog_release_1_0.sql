da6d3316d1474744876c708e820ced93f4f1aa5e
jkowalski2 <r.grzegorczyk@hotmail.com>
2021-06-20 20:20:47 +0200

    [feat] [Release_1_0] [JIRA-2] Added shoes range and model column

--- HR/releases/release_1_0/changelog_release_1_0.sql
+++ HR/releases/release_1_0/changelog_release_1_0.sql
@@ -1,1 +1,8 @@
 --liquibase formatted sql
+--changeset JKOWALSKI2:Add_shoes_rangeand_model context:RELEASE_1_0 labels:JIRA-2
+--comment added new columns
+ALTER TABLE HR.SHOES ADD (
+  MIN_NUMBER NUMBER,
+  MAX_NUMBER NUMBER,
+  MODEL VARCHAR2(50)
+);
